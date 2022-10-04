6b6e9394857449b24b43a68e5547c1f420580378
kashifkhan0336 <kashifkhan0336@gmail.com>
2022-10-04 08:29:36 +0500

    init2

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -6,6 +6,8 @@ use App\Filament\Resources\ProductResource\Pages;
 use App\Filament\Resources\ProductResource\RelationManagers;
 use App\Models\Product;
 use Filament\Forms;
+use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
+use Filament\Forms\Components\Toggle;
 use Filament\Resources\Form;
 use Filament\Resources\Resource;
 use Filament\Resources\Table;
@@ -18,10 +20,7 @@ class ProductResource extends Resource
     protected static ?string $model = Product::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-collection';
-    protected static function getNavigationBadge(): ?string
-    {
-        return static::getModel()::count();
-    }
+
     public static function form(Form $form): Form
     {
         return $form
@@ -30,44 +29,70 @@ class ProductResource extends Resource
                     ->relationship('category', 'name')
                     ->required(),
                 Forms\Components\TextInput::make('name')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('desc')
-                    ->maxLength(255),
+                    ->required(),
+                Forms\Components\RichEditor::make('desc')
+                    ->required(),
+
+
                 Forms\Components\TextInput::make('price')
                     ->required(),
+                Forms\Components\TextInput::make('stock')
+                    ->required(),
                 Forms\Components\TextInput::make('product_number')
-                    ->required(),
+                    ->required()
+                    ->minValue(5)
+                    ->maxValue(5)
+                    ->unique(ignorable: fn ($record) => $record),
                 Forms\Components\TextInput::make('product_code')
-                    ->required(),
+                    ->required()
+                    ->minValue(2)
+                    ->maxValue(2),
+                Forms\Components\Section::make('Images')
+                    ->schema([
+                        SpatieMediaLibraryFileUpload::make('media')
+                            ->collection('main-image')
+                            ->responsiveImages()
+                            ->disableLabel(),
+                    ])
+                    ->collapsible(),
+
                 Forms\Components\DateTimePicker::make('added')
                     ->required(),
+                Forms\Components\Section::make('Toggles')
+                    ->schema([
+                        Toggle::make('is_featured')->inline()
+                    ])
+                    ->collapsible(),
+
             ]);
     }
-
     public static function table(Table $table): Table
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('category_id'),
-                Tables\Columns\TextColumn::make("category.name"),
+                Tables\Columns\SpatieMediaLibraryImageColumn::make('product-image')
+                    ->label('Image')
+                    ->collection('main-image'),
+                Tables\Columns\TextColumn::make('category.name'),
                 Tables\Columns\TextColumn::make('name'),
                 Tables\Columns\TextColumn::make('desc'),
                 Tables\Columns\TextColumn::make('price'),
+                Tables\Columns\TextColumn::make('stock'),
                 Tables\Columns\TextColumn::make('product_number'),
                 Tables\Columns\TextColumn::make('product_code'),
+                Tables\Columns\TextColumn::make('main_image'),
                 Tables\Columns\TextColumn::make('added')
                     ->dateTime(),
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime(),
                 Tables\Columns\TextColumn::make('updated_at')
                     ->dateTime(),
+                Tables\Columns\ToggleColumn::make('is_featured')
             ])
             ->filters([
                 //
             ])
             ->actions([
-                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
@@ -87,7 +112,6 @@ class ProductResource extends Resource
         return [
             'index' => Pages\ListProducts::route('/'),
             'create' => Pages\CreateProduct::route('/create'),
-            'view' => Pages\ViewProduct::route('/{record}'),
             'edit' => Pages\EditProduct::route('/{record}/edit'),
         ];
     }
